59c80f666a80881a9aa0cf147290c5f1e0b8a54e
mbobrowska93 <m.bobrowska93@wp.pl>
2020-09-10 21:07:03 +0200

    code refactoring

--- src/app/products.service.spec.ts
+++ src/app/products.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
 
-describe('OurProductsService', () => {
+describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
